533e9d3e64f851e6e4a78e25aa03780b84d1fc61
enaut-genua <enaut.genua@alumni.mondragon.edu>
2025-10-26 18:53:05 +0100

    Try fixing function registering issue

--- src/payment/messaging/__init__.py
+++ src/payment/messaging/__init__.py
@@ -2,12 +2,14 @@ from .global_vars import (
     LISTENING_QUEUES,
     RABBITMQ_CONFIG,
 )
+from . import events
 from typing import (
     List,
-    LiteralString
+    LiteralString,
 )
 
 __all__: List[LiteralString] = [
+    "events",
     "LISTENING_QUEUES",
     "RABBITMQ_CONFIG",
 ]
